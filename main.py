984c1bc60fbd116320c80ace4b28245e2b5e5190
LionelRohner <lionel.rohner@gmail.com>
2025-08-11 22:51:18 -0400

    work out logic for scale building

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 class Scale:
+    scales_dict = {
+        "major": [0, 1, 1, 0.5, 1, 1, 1, 0.5],
+        # basically minor is a rotation of major, like all other modes
+        # therefor make generic
+        "minor": [0, 1, 1, 0.5, 1, 1, 0.5, 1],
+    }
+    scales_dict = {key: [x * 2 for x in value] for key, value in scales_dict.items()}
+
+    notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"] * 3
+
     def __init__(self, root_note, scale_type):
         self.root_note = root_note
         self.scale_type = scale_type
         self.notes = self.generate_scale()
 
     def generate_scale(self):
-        pass
+        print(self.notes)
+        # scale = Scale.scales_dict[self.scale_type]
+        # continue here
+
 
 class Chord:
     def __init__(self, root_note, chord_type):
@@ -15,8 +28,10 @@ class Chord:
     def display_chord(self):
         pass
 
+
 class Mode(Scale):
     pass
+
 
 class ChordProgressionGenerator:
     def __init__(self, scale):
@@ -25,11 +40,11 @@ class ChordProgressionGenerator:
     def get_chord_progression(self):
         print(self.scale)
 
-# Example usage
-c_major_scale = Scale("C", "major")
-chord_progression_generator = ChordProgressionGenerator(c_major_scale)
-chords = chord_progression_generator.get_chord_progression()
 
-print(c_major_scale)
-print(chord_progression_generator)
-print(chords)
+# Example usage
+scale = Scale("C", "major")
+# print(scale.scales_dict)
+
+
+chord_progression_generator = ChordProgressionGenerator(scale)
+chords = chord_progression_generator.get_chord_progression()
