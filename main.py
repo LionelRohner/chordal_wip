437206055d80ae5eb7900cade69cb9b7c53d0b62
LionelRohner <lionel.rohner@gmail.com>
2025-08-14 14:08:05 -0400

    add todo file

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from chordal_wip.scales import Scale
+from chordal_wip.scales import Scale, rotate_list
 
-scale = Scale("C", "ionian")
 
+scale_ref = Scale("C", "ionian")
+print(scale_ref.notes)
+
+
+scale = Scale("F#", "mixolydian")
 print(scale.notes)
