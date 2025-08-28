eb3c9ac55e54a8e5919abc5e61c7e0b8b2e50626
LionelRohner <lionel.rohner@gmail.com>
2025-08-27 20:20:47 -0400

    add crappy logic for random chord generation
    * needs revision

--- main.py
+++ main.py
@@ -9,7 +9,19 @@ chord = Chord(scale_ref)
 
 chord_progression = ChordProgression(n_chords=4, chord=chord)
 
-print(vars(chord_progression))
+# print(vars(chord_progression))
+dd = chord_progression.progression
+# print([chord for chord in dd if chord["name"] == "tonic"])
+print(dd)
+print([chord["name"] for chord in dd if chord])
+print(any(chord["name"] == "tonic" for chord in dd))
+# [d["name"].values() for d in dd.values()]
+# lst = [{"a": "str1", "b": 1}, {"a": "str2", "b": 2}, {"a": "str3", "b": 3}]
+
+# print(sum(d["b"] for d in lst))
+# print([sum(v for v in d["b"]) for d in lst])
+
+# print(vars(chord_progression))
 
 # scale = Scale("F#", "mixolydian")
 # print("F mixolydian:", scale.notes)
