1509e4f47ef6b347aaa16cbecbb6126337aefc95
LionelRohner <lionel.rohner@gmail.com>
2025-11-28 02:46:51 -0500

    add function to homogenize chords with 2 extensions

--- chordal_wip/chordcleaner.py
+++ chordal_wip/chordcleaner.py
@@ -39,22 +39,42 @@ class ChordCleaner:
         return txt
 
     def _negative_selection(self, txt):
-        print(f"threshold: {self.threshold}")
         counts = txt.str.split(" ").explode().value_counts()
-        print(f"counts:\n {counts}")
+
         counts_filtered = counts[counts <= self.threshold].index
         # re.sub acts sequentially, hence this sorting avoids filtering substrings
         counts_filtered = sorted(counts_filtered, key=len, reverse=True)
         counts_filtered = [re.escape(pattern) for pattern in counts_filtered]
-        print(f"counts_filtered:\n {counts_filtered}")
+
         neg_selection_patterns = f"({('|').join(counts_filtered)})"
-        print(f"neg_selection_patterns:\n {neg_selection_patterns}")
         txt = txt.apply(lambda x: re.sub(neg_selection_patterns, "", x).strip())
         return txt
 
-    def _filter_chords(self, text):
+    def _clean_double_extensions(self, txt):
+        # Convert slash notation to parenthesis notation
+        slash_pattern = r"(\w+\d+)/(\w+\d+)"
+        txt = re.sub(slash_pattern, r"\1(\2)", txt)
+        # Convert add notation to parenthesis notation
+        add_pattern = r"([A-G]{1}[#b]?[Majmdinsu]{0,3}\d{1,2})add(\d{1,2})"
+        txt = re.sub(add_pattern, r"\1(\2)", txt)
+        return txt
+
+    def _filter_chords(self, txt):
         """Filter chords using a regex pattern."""
+        # Anatomy of a chord
+        root = "[A-G]"
+        accidental = "[#b]?"
+        note = f"({root}{accidental})"
+        quality = "(M|Maj|maj|m|min|dim|sus|add|aug)?"
+        extension = "([1-9]|1[0-3])?"
+        extension2 = f"(\({accidental}{extension}\))?"
+        slash = f"(\/{note})?"
+        chord_anatomy = (
+            f"^{root}{accidental}{quality}{extension}{extension2}{slash}$"
+        )
         # pattern = "^[A-G][#,b]?([1-9]|1[0-3])?(M|Maj|maj|m|min|dim|sus|add|aug)?([1-9]|1[0-3])?[\/]?([A,B,C,D,E,F,G]?[#,b]?|[1-9])"
+        print(chord_anatomy)
+        print(re.match(chord_anatomy, txt))
         pass
 
     # Tab filter >> maybe use "-*"
