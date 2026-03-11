fd6b32472f5112e02af78d6df83c13d3f75493ee
LionelRohner <lionel.rohner@gmail.com>
2026-03-10 23:53:53 -0400

    add new tests for canonicalizer

--- tests/test_chordcanonizer.py
+++ tests/test_chordcanonizer.py
@@ -39,6 +39,15 @@ def test_decompose_2():
     assert actual == expected, f"Expected {expected}, got {actual}"
 
 
+def test_num_sort():
+    test = ["add9", "add2", "add13", "X"]
+
+    actual = [cc._num_sort(i) for i in test]
+    expected = [9, 2, 13, 999]
+
+    assert actual == expected, f"Expected {expected}, got {actual}"
+
+
 def test_canonicalize_length_conservation():
     test = "not_chord exit progression lit effect"
 
@@ -57,6 +66,15 @@ def test_canonicalize_triads():
     assert actual == expected, f"Expected {expected}, got {actual}"
 
 
+def test_canonicalize_aug():
+    test = "C5+ C+5 C7+ Caug C+"
+
+    actual = cc.canonicalize(test)
+    expected = ["Caug5", "Caug5", "Caug7", "Caug", "Caug"]
+
+    assert actual == expected, f"Expected {expected}, got {actual}"
+
+
 def test_canonicalize_7th():
     test = "Emaj7 Emin7 Em7 E7M EM7 E7"
 
@@ -70,6 +88,34 @@ def test_canonicalize_7th_2():
     test = "Gmaj9 Gmaj7 Gmaj7(9+)"
 
     actual = cc.canonicalize(test)
-    expected = ["Gmaj9", "Gmaj7", "Gmaj7(9#)"]
+    expected = ["Gmaj9", "Gmaj7", "Gmaj7(e:#9)"]
+
+    assert actual == expected, f"Expected {expected}, got {actual}"
+
+
+def test_canonicalize_extensions():
+    test = "C7/9- C9#11b13 C911+13-"
+
+    actual = cc.canonicalize(test)
+    expected = ["C7(e:b9)", "C9(e:#11,b13)", "C9(e:#11,b13)"]
+
+    assert actual == expected, f"Expected {expected}, got {actual}"
+
+
+def test_canonicalize_extensions_edge_cases():
+    """
+    Leading accidentals "+" and "-" are not allowed.
+    Trailing accidentals "#" and "b" are not allowed.
+    Cave: Two trailing accidentals using "#" and "b" are wronly interpreted as valid cases, because of the above rules!
+    """
+    test = "C7/-9 C7/9b C7/9# C911#13b"
+
+    actual = cc.canonicalize(test)
+    expected = [
+        "C7(u:-9)",  # Leading "-" is not expected in token splitting criteria
+        "C7(e:b9)",  # TODO: find out why 9b is rotated
+        "C7(e:#9)",  # TODO: find out why 9# is rotated
+        "C9(e:11,#13)",  # Trailing # or b are not expected, hence it favors #13 instead of 11# and 13b.
+    ]
 
     assert actual == expected, f"Expected {expected}, got {actual}"
